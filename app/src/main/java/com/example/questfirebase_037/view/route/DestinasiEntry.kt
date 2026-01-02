c4e9e9bbbd3b0228d5abc700dec04d83d345ab64
prdpta <pradiptaputramei01@gmail.com>
2026-01-02 21:48:11 +0700

    peta alamat sederhana untuk halaman Tambah Siswa (Entry) yang digunakan sebagai tujuan navigasi saat Anda ingin memasukkan data baru ke aplikasi

---
+++ app/src/main/java/com/example/questfirebase_037/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase_037.view.route
+
+import com.example.questfirebase_037.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
