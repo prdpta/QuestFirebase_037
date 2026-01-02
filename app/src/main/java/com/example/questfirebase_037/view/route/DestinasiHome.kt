6f82e66fa6f7287a1e6816354b24fda5c619cf2b
prdpta <pradiptaputramei01@gmail.com>
2026-01-02 21:48:58 +0700

    peta alamat untuk Halaman Utama (Beranda) yang berfungsi sebagai titik awal atau tampilan pertama saat aplikasi dibuka

---
+++ app/src/main/java/com/example/questfirebase_037/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase_037.view.route
+
+import com.example.questfirebase_037.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
