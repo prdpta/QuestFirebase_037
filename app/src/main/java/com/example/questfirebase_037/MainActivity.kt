7cc50df854a0ae40ee1fce1b271f5ca6109d2d6c
prdpta <pradiptaputramei01@gmail.com>
2026-01-02 23:08:57 +0700

    menambahkan MainActivity.kt

--- app/src/main/java/com/example/questfirebase_037/MainActivity.kt
+++ app/src/main/java/com/example/questfirebase_037/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.questfirebase_037.ui.theme.QuestFirebase_037Theme
+import com.example.questfirebase_037.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +18,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestFirebase_037Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestFirebase_037Theme {
-        Greeting("Android")
     }
 }
