1638527d839952119cf442e6f54a6d78d4edc3f6
prdpta <pradiptaputramei01@gmail.com>
2026-01-02 20:36:41 +0700

    untuk mengatur agar aplikasi Anda rapi, mudah diatur sumber datanya, dan efisien dalam penggunaan memori

--- app/src/main/java/com/example/questfirebase_037/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questfirebase_037/repositori/ContainerApp.kt
@@ -12,3 +12,10 @@ class DefaultContainerApp : ContainerApp {
     }
 }
 
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
