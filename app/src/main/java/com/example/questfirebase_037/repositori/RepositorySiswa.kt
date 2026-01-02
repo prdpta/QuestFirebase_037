62e718780af62662a9dfb46b8acc853dba3f92cb
prdpta <pradiptaputramei01@gmail.com>
2026-01-02 20:39:06 +0700

    kontrak kerja yang mengatur cara aplikasi mengambil dan mengirim data siswa secara aman di latar belakang

---
+++ app/src/main/java/com/example/questfirebase_037/repositori/RepositorySiswa.kt
@@ -0,0 +1,11 @@
+package com.example.questfirebase_037.repositori
+
+import com.example.questfirebase_037.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+interface RepositorySiswa {
+    suspend fun getDataSiswa(): List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
+
