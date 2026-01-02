7a3106d84f78cb090328d29621d4572b6a9f9195
prdpta <pradiptaputramei01@gmail.com>
2026-01-02 20:24:42 +0700

    berfungsi sebagai wadah data untuk objek Siswa

---
+++ app/src/main/java/com/example/questfirebase_037/modeldata/Siswa.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_037.modeldata
+
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
