fa1d417c7e1a02fbb330e614c0e43e429e81f127
prdpta <pradiptaputramei01@gmail.com>
2026-01-02 21:46:45 +0700

    definisi alamat (rute) khusus untuk halaman Detail Siswa agar navigasi bisa mengirimkan data spesifik (seperti ID) saat berpindah layar

---
+++ app/src/main/java/com/example/questfirebase_037/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.questfirebase_037.view.route
+
+import com.example.questfirebase_037.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
