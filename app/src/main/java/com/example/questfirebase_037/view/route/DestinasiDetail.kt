18797bd84cfc0f1990aedabc38fa04a85a3c33cb
prdpta <pradiptaputramei01@gmail.com>
2026-01-09 20:59:57 +0700

    berfungsi sebagai pendefinisi rute navigasi halaman detail siswa yang menyimpan nama rute, judul halaman, serta parameter idSiswa untuk menampilkan detail data siswa tertentu

--- app/src/main/java/com/example/questfirebase_037/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questfirebase_037/view/route/DestinasiDetail.kt
@@ -2,7 +2,7 @@ package com.example.questfirebase_037.view.route
 
 import com.example.questfirebase_037.R
 
-object DestinasiDetail : DestinasiNavigasi {
+object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
     const val itemIdArg = "idSiswa"
