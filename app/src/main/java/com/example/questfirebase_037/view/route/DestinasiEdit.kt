c0ba23d816cc148ec892e7ad2b9d4bf3fe40777a
prdpta <pradiptaputramei01@gmail.com>
2026-01-09 20:59:25 +0700

    berfungsi sebagai pendefinisi rute navigasi halaman edit siswa yang menyimpan nama rute, judul halaman, serta parameter idSiswa untuk mengidentifikasi data siswa yang akan diedit

--- app/src/main/java/com/example/questfirebase_037/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questfirebase_037/view/route/DestinasiEdit.kt
@@ -2,7 +2,7 @@ package com.example.questfirebase_037.view.route
 
 import com.example.questfirebase_037.R
 
-object DestinasiEdit : DestinasiNavigasi {
+object DestinasiEdit: DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
     const val itemIdArg = "idSiswa"
